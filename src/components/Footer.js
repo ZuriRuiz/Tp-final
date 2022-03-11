2bdf1a159299f733d85babd10fba29c5d9413e98
Zuri <freyahr@gmail.com>
2022-03-10 23:15:00 -0300

    se agrego footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,25 @@
-const Footer = () => {};
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
+
+
+const Footer = () => {
+    return(
+        <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          
+        <Typography variant="h8" sx={{ flexGrow: 1 }}>
+            Creado por Zuri 
+        </Typography>
+         
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+    
 export default Footer;
