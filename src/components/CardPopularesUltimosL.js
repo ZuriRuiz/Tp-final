813cab8b036e9022e65be26d58cc5c22edf6aa34
Zuri <freyahr@gmail.com>
2022-03-25 23:25:09 -0300

    se agrego el estilado de la tarjeta de los componentes populares y ultimos lanzamientos

--- src/components/CardPopularesUltimosL.js
+++ src/components/CardPopularesUltimosL.js
@@ -1,15 +1,43 @@
-import Typography from '@mui/material/Typography';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
 
+const CardPopularesUltimosL = ({ titulo, imagen, link }) => {
+  return (
+    
+        <Card
+          sx={{
+            width: 250,
+                height: 450,
+                m: 2, 
+                display:"flex",
+                flexDirection: "column"
+          }}
+        >
+          <CardMedia
+            component="img"
+            height="300"
+            image={imagen}
+            alt={`Img-${titulo}`}
+          />
 
-const CardPopularesUltimosL = ({titulo, imagen, link}) => {
-    return (
-        <div>
-        <Typography sx={{ pl: 1, pt: 1, }}>{titulo}</Typography>
-        <img src = {imagen}></img>
-        
-        </div>
-    )
-}
-
+          <CardContent>
+            <Typography gutterBottom variant="h5">
+              {titulo}
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
+     
+  );
+};
 
 export default CardPopularesUltimosL;
